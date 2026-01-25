6e5ed1e3eb6657aa89706215feb7b0fb61752752
trpdjke <yehorushakov@gmail.com>
2026-01-25 19:23:16 +0200

    megre to pgx/v5

--- transaction.go
+++ transaction.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -21,18 +22,16 @@ func From(ctx context.Context) (pgx.Tx, bool) {
 }
 
 type DBTX interface {
-	Exec(context.Context, string, ...any) (pgconnCommandTag, error)
+	Exec(context.Context, string, ...any) (pgconn.CommandTag, error)
 	Query(context.Context, string, ...any) (pgx.Rows, error)
 	QueryRow(context.Context, string, ...any) pgx.Row
 }
-
-type pgconnCommandTag = pgx.CommandTag
 
 type poolDBTX struct {
 	pool *pgxpool.Pool
 }
 
-func (p poolDBTX) Exec(ctx context.Context, sql string, args ...any) (pgconnCommandTag, error) {
+func (p poolDBTX) Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error) {
 	return p.pool.Exec(ctx, sql, args...)
 }
 
@@ -61,19 +60,27 @@ func Transaction(pool *pgxpool.Pool, ctx context.Context, fn func(ctx context.Co
 		return err
 	}
 
+	committed := false
 	defer func() {
 		if p := recover(); p != nil {
 			_ = tx.Rollback(ctx)
 			panic(p)
+		}
+		if !committed {
+			_ = tx.Rollback(ctx)
 		}
 	}()
 
 	ctx = Inject(ctx, tx)
 
 	if err = fn(ctx); err != nil {
-		_ = tx.Rollback(ctx)
 		return err
 	}
 
-	return tx.Commit(ctx)
+	if err = tx.Commit(ctx); err != nil {
+		return err
+	}
+
+	committed = true
+	return nil
 }
